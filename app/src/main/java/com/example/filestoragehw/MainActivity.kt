8db74b41504ef4b12fb1505aabb5ca2e80b698c0
evan patterson <thedeepdaemon@gmail.com>
2022-03-21 14:56:53 -0400

    pretty much did it

--- app/src/main/java/com/example/filestoragehw/MainActivity.kt
+++ app/src/main/java/com/example/filestoragehw/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.filestoragehw
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val retButton: Button = findViewById<Button>(R.id.retrieve_button)
+
+
+        retButton.setOnClickListener {
+            val msg: String = constructMessage()
+            val tv: TextView = findViewById(R.id.textView)
+            tv.text = msg
+        }
+
     }
+
+    private fun constructMessage(): String {
+        val dir: String = Environment
+            .getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS)
+            .toString()
+        return "External files are stored at:\n$dir"
+    }
+
 }
